ba445fe978400a3373f2cf5f3a3601974211d761
jeffrey-zang <jeffrey.zang.07@gmail.com>
2024-06-08 22:41:30 -0400

    init: project with react tw radix

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import '@radix-ui/themes/styles.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="font-bold text-3xl">Your fractions.</h1>
+      <h1>Simplified.</h1>
+    </div>
+  )
+}
+
+export default App
