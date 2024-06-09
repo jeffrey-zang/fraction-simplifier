d32e842118104fada20a7406192c5def5ebc2d39
jeffrey-zang <jeffrey.zang.07@gmail.com>
2024-06-09 09:16:39 -0400

    task: primitive UI using radix
    Co-authored-by: Gary Zhang <GaryZhang51@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-// import '@radix-ui/themes/styles.css';
+import { Button, Card } from "@radix-ui/themes"
+import { useState } from "react"
 
 const App = () => {
+
+  const [denominator, setDenominator] = useState('')
+  const [numerator, setNumerator] = useState('')
+  const [result, setResult] = useState('')
+  const [error, setError] = useState('')
+
   return (
-    <div>
-      <h1 className="font-bold text-3xl">Your fractions.</h1>
-      <h1>Simplified.</h1>
+    <div className="grid place-items-center h-screen">
+      <Card size="3">
+        <h1 className="font-bold text-3xl">Your fractions.</h1>
+        <h1>Simplified.</h1>
+
+        <form>
+          <input type='text' placeholder='Denominator' />
+          <input type='text' placeholder='Numerator' />
+          <Button>Submit</Button>
+        </form>
+      </Card>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import '@radix-ui/themes/styles.css';
+import { Theme } from '@radix-ui/themes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Theme
+      accentColor='indigo'
+      grayColor='slate'
+      panelBackground='translucent'
+      radius='large'
+    >
+      <App />
+    </Theme>
   </React.StrictMode>,
 )
